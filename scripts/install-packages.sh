754230c4284992ebaeb244ea99e15d14efff4731
Tam Long <aqua.tamlong@gmail.com>
2023-03-09 16:39:15 +0700

    Init

---
+++ scripts/install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+sudo pacman -S \
+	xorg \
+	xorg-xinit \
+	bspwm \
+	sxhkd \
+	picom \
+	nitrogen \
+	alacritty \
+	rofi \
+	polybar \
+	thunar
---
+++ scripts/setup-audio.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# This script is used to setup audio and speaker on this machine
+sudo pacman -S pipewire pipewire-pulse
